852ca4e1f19bcd9ee7cada4618ad5f19d737cc73
Sebastian Borrazas <seba.borrazas@gmail.com>
2017-11-19 19:08:14 -0300

    Mejorar manejo de logs: errors y warnings

--- src/utils/debug.h
+++ src/utils/debug.h
@@ -1,19 +1,19 @@
+#ifndef __DEBUG_H__
+#define __DEBUG_H__
+
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
-
-#ifdef NDEBUG
-#define debug(M, ...)
-#else
-#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n", __FILE__, __LINE __, ##__VA_ARGS__)
-#endif
+#include <error.h>
 
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
 
-#define log_err(status, M, ...) error_at_line(status, errno, __FILE__, __LINE__, "[ERROR] (%s:%d: errno: %s) " M "\n", clean_errno(), ##__VA_ARGS__)
+#define log_err(M, ...) error_at_line(EXIT_FAILURE, errno, __FILE__, __LINE__, "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
 
-#define log_warn(M, ...) fprintf(stderr, "[WARN] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+#define log_warn(M, ...) error_at_line(EXIT_SUCCESS, errno, __FILE__, __LINE__, "[WARN] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
 
 #define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n", __FILE__, _ _LINE__, ##__VA_ARGS__)
 
 #define assert(A, M, ...) if(!(A)) { log_err(M, ##__VA_ARGS__); errno=0; }
+#endif
+
