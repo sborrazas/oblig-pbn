bf956b3d541fc167d8000af4c9698abb63f65e9f
Sebastian Borrazas <seba.borrazas@gmail.com>
2017-11-26 19:49:36 -0300

    Incrementar indices de forma circular

--- src/modules/queue_mem.c
+++ src/modules/queue_mem.c
@@ -1,6 +1,7 @@
 #include "queue_mem.h"
 
 short int is_queue_full(Queue_Mem* mem);
+void increase_index(int* index, Queue_Mem* mem);
 
 /******************************************************************************/
 /* SARED_MEM ******************************************************************/
@@ -122,29 +123,29 @@ void queue_mem_add_msg(Queue_Mem* mem, int semid, Message* msg) {
     if (is_queue_full(mem)) {
         if (msg->high_priority) {
             mem->hp_messages.messages[mem->hp_messages.end_index] = *msg;
-            mem->hp_messages.end_index++;
+            increase_index(&mem->hp_messages.end_index, mem);
             if (mem->lp_messages.count > 0) {
-                mem->lp_messages.start_index++;
+                increase_index(&mem->lp_messages.start_index, mem);
                 mem->hp_messages.count++;
                 mem->lp_messages.count--;
             }
         }
         else {
             mem->lp_messages.messages[mem->lp_messages.end_index] = *msg;
-            mem->lp_messages.end_index++;
-            mem->lp_messages.start_index++;
+            increase_index(&mem->lp_messages.end_index, mem);
+            increase_index(&mem->lp_messages.start_index, mem);
         }
         sem_v(BUFFER_SEMAPHORE, semid);
     }
     else {
         if (msg->high_priority) {
             mem->hp_messages.messages[mem->hp_messages.end_index] = *msg;
-            mem->hp_messages.end_index++;
+            increase_index(&mem->hp_messages.end_index, mem);
             mem->hp_messages.count++;
         }
         else {
             mem->lp_messages.messages[mem->lp_messages.end_index] = *msg;
-            mem->lp_messages.end_index++;
+            increase_index(&mem->lp_messages.end_index, mem);
             mem->lp_messages.count++;
         }
 
@@ -159,12 +160,12 @@ void queue_mem_remove_msg(Queue_Mem* mem, int semid, Message* msg) {
 
     if (mem->hp_messages.count > 0) {
         *msg = mem->hp_messages.messages[mem->hp_messages.start_index];
-        mem->hp_messages.start_index++;
+        increase_index(&mem->hp_messages.start_index, mem);
         mem->hp_messages.count--;
     }
     else {
         *msg = mem->lp_messages.messages[mem->lp_messages.start_index];
-        mem->lp_messages.start_index++;
+        increase_index(&mem->lp_messages.start_index, mem);
         mem->lp_messages.count--;
     }
 
@@ -173,4 +174,10 @@ void queue_mem_remove_msg(Queue_Mem* mem, int semid, Message* msg) {
 
 short int is_queue_full(Queue_Mem* mem) {
     return (mem->hp_messages.count + mem->lp_messages.count) == mem->max_messages;
+}
+
+void increase_index(int* index, Queue_Mem* mem) {
+    int queues_size = mem->max_messages;
+
+    *index = (*index + 1) % queues_size;
 }
